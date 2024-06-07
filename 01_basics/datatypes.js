8be4ef322bf6cac61717a97aa7206db62759fe1b
Rojan7 <120560160+Rojan7@users.noreply.github.com>
2024-06-07 05:25:32 +0000

    day1commit

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"//this will treat alal javascripgt code as newer version code
+// alert(3+3)//this will not give the output as we are not running it on browser rather we can use this
+console.log(3+3)
+//  datatypes in javascript
+let name ="rojan"
+let age = 18
+let isloggedin = false
+let state;
+
+//number data type => range till 2 to the power 53 
+//if we have to use larger datas we can use long int
+// string =""
+//boolean = true/ false
+//null = standalone value
+//undefined =>
+//symnbol => unique
+
+//object
+console.log(typeof undefined);//undefined
+console.log (typeof null);//object
---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 8566
+let accountemail = "rojanadhikari02@gmail.com"
+var accountpassword = "dunno"
+accountcity = "biratnagar"
+let bannorebanno;
+
+accountemail = "rojanadhikari02@female.com"
+accountpassword = "benno"
+accountcity ="dharan"
+
+console.log(accountId);
+
+// we donot use var in javascript as it gives us scope related issues
+
+console.table([accountemail,accountpassword,accountcity,bannorebanno])
